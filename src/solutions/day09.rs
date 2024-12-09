54a32e8dc8ee8cca6b8f9940ced5a87ef942bc0b
jp <jp@finoto.dev>
2024-12-09 09:04:37 +0100

    feat(day09): add day 9 part 2 solution

--- src/solutions/day09.rs
+++ src/solutions/day09.rs
@@ -1,11 +1,23 @@
 use crate::aoc::*;
 use crate::solution;
-use std::collections::VecDeque;
+use itertools::Itertools;
+use std::collections::{LinkedList, VecDeque};
 use std::iter;
 
 const DAY: Day = 9;
 
-solution!(DAY, solve_part_1);
+solution!(DAY, solve_part_1, solve_part_2);
+
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+struct File {
+    id: usize,
+    length: usize,
+}
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+enum Block {
+    Empty(usize),
+    File(File),
+}
 
 fn solve_part_1(input: impl Lines) -> usize {
     let data = parse(&input);
@@ -14,16 +26,62 @@ fn solve_part_1(input: impl Lines) -> usize {
         None => files_only.pop_back(),
         Some(_) => files_only.pop_front(),
     });
-    checksum(result.flatten())
+    checksum(result)
 }
 
-#[allow(unused)]
-fn solve_part_2(input: impl Lines) -> i64 {
-    0
+fn solve_part_2(input: impl Lines) -> usize {
+    let mut blocks = parse_2(&input);
+    let mut files_only: Vec<_> = blocks
+        .clone()
+        .into_iter()
+        .flat_map(|block| match block {
+            Block::Empty(_) => None,
+            Block::File(file) => Some(file),
+        })
+        .rev()
+        .collect();
+    for file in &mut files_only {
+        let position = blocks
+            .iter()
+            .find_position(|x| **x == Block::File(*file))
+            .unwrap()
+            .0;
+        let mut tail = blocks.split_off(position);
+        tail.pop_front();
+        blocks.extend(iter::once(Block::Empty(file.length)));
+        blocks.append(&mut tail);
+        let mut insert_pos = None;
+        for (i, block) in blocks.iter().enumerate() {
+            match block {
+                Block::Empty(length) if *length >= file.length => {
+                    insert_pos = Some(i);
+                    break;
+                }
+                _ => {}
+            }
+        }
+        let insert_pos = insert_pos.unwrap();
+        let mut tail = blocks.split_off(insert_pos);
+        let Block::Empty(empty_block_length) = tail.pop_front().unwrap() else {
+            panic!("Block isn't empty?")
+        };
+        blocks.extend(iter::once(Block::File(*file)));
+        if empty_block_length > file.length {
+            blocks.extend(iter::once(Block::Empty(empty_block_length - file.length)));
+        }
+        blocks.append(&mut tail);
+    }
+    checksum(blocks.iter().flat_map(|block| match block {
+        Block::Empty(length) => iter::repeat_n(None, *length),
+        Block::File(file) => iter::repeat_n(Some(file.id), file.length),
+    }))
 }
 
-fn checksum(values: impl Iterator<Item = usize>) -> usize {
-    values.enumerate().map(|(i, value)| i * value).sum()
+fn checksum(values: impl Iterator<Item = Option<usize>>) -> usize {
+    values
+        .enumerate()
+        .filter_map(|(i, value)| value.map(|v| v * i))
+        .sum()
 }
 
 fn parse(input: &impl Lines) -> Vec<Option<usize>> {
@@ -43,6 +101,27 @@ fn parse(input: &impl Lines) -> Vec<Option<usize>> {
         .collect()
 }
 
+fn parse_2(input: &impl Lines) -> LinkedList<Block> {
+    input
+        .get_raw()
+        .trim()
+        .chars()
+        .enumerate()
+        .flat_map(|(i, c)| {
+            let length = c.to_digit(10).unwrap() as usize;
+            if length > 0 {
+                if i % 2 == 0 {
+                    Some(Block::File(File { id: i / 2, length }))
+                } else {
+                    Some(Block::Empty(length))
+                }
+            } else {
+                None
+            }
+        })
+        .collect()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -57,6 +136,6 @@ mod tests {
 
     #[test]
     fn test_part_2() {
-        aoc_test!(DAY, 2, 0, TEST_INPUT);
+        aoc_test!(DAY, 2, 2858, TEST_INPUT);
     }
 }
